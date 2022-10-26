88725e488a2b9b94c7d6e771d9c94af50f0ec1df
liia.stepanova <Liia.Stepanova@Tideworks.com>
2022-10-26 14:50:24 +0300

    fixed

--- src/main/java/com/example/demo/repository/LocationRepository.java
+++ src/main/java/com/example/demo/repository/LocationRepository.java
@@ -1,9 +1,6 @@
 package com.example.demo.repository;
 
-import java.util.List;
-import java.util.Optional;
 
-import com.example.demo.entity.Animal;
 import com.example.demo.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
-    List<Animal> findAllByAnimal(Animal animal);
-
-    Optional<Animal> findLocationById(Long id);
 }
